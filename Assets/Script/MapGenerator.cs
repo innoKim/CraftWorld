0e0b7a8528f425a8e3654f4adc24c036316cc40e
KimInho <idhik@naver.com>
2017-11-07 21:57:24 +0900

    블랜더로 만든 큐브 메쉬 테스트
    결과 가장 뛰어난 퍼포먼스를 보여줌 좀더 다듬은 뒤에 텍스쳐 구해서 사용하는걸로

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
-    public List<GameObject> Blocks;
+    private enum ObjType
+    {
+        Soil,
+        Sand,
+        Gravel,
+        Rock,
+        Metal,
+        Water,
+        None,
+    }
 
+    public List<GameObject> Blocks;
+    
     public int MapWidth;
     public int MapDepth;
     public int WaterLevelHeight;
@@ -19,6 +30,7 @@ public class MapGenerator : MonoBehaviour {
     public int xSeed;
     public int zSeed;
 
+    private ObjType[,,] objmapArr;
 
     private float[,] heightArr;
     
@@ -39,13 +51,47 @@ public class MapGenerator : MonoBehaviour {
         Map.isStatic = true;
 
         NoiseGenerate(0, 0);
+
+        ObjMapGenerate();
         BlockGenerate();
+    }
+
+    void ObjMapGenerate()
+    {
+        objmapArr = new ObjType[MapWidth, (int)heightScale, MapDepth];
+
+        for (int x = 0; x < MapWidth; x++)
+        {
+            for (int y = 0; y < heightScale; y++)
+            {
+                for (int z = 0; z < MapWidth; z++)
+                {
+                    objmapArr[x, y, z] = ObjType.None;
+                }
+            }
+        }
+
+        GroundGenerate();
+        MetalGenerate();
+        RockGenerate();        
+        WaterGenerate();
     }
 
     void BlockGenerate()
     {
-        GroundGenerate();
-        WaterGenerate();
+        for (int x = 0; x < MapWidth; x++)
+        {
+            for (int y = 0; y < heightScale; y++)
+            {
+                for (int z = 0; z < MapWidth; z++)
+                {
+                    if(objmapArr[x,y,z] !=ObjType.None)
+                    {
+                        GameObject newBlock = Instantiate(Blocks[(int)objmapArr[x, y, z]], new Vector3(x, y, z), Quaternion.identity, Map.transform);
+                    }
+                }
+            }
+        }
     }
 
     void GroundGenerate()
@@ -56,12 +102,41 @@ public class MapGenerator : MonoBehaviour {
             {
                 for (int y = 0; y < heightArr[x, z]; y++)
                 {
-                    GameObject newBlock = Instantiate(Blocks[0], new Vector3(x, y, z), Quaternion.identity, Map.transform);
-                }
+                    objmapArr[x, y, z] = ObjType.Soil;
+                }               
             }
         }
     }
 
+    void MetalGenerate()
+    {
+        for(int i=0;i<100;i++)
+        {
+            int xRand = Random.Range(0, MapWidth);
+            int yRand = (int)Random.Range(0, heightScale);
+            int zRand = Random.Range(0, MapDepth);
+
+            if (objmapArr[xRand,yRand,zRand]!=ObjType.None)
+            {
+                objmapArr[xRand, yRand, zRand] = ObjType.Metal;
+            }
+        }
+    }
+
+    void RockGenerate()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            int xRand = Random.Range(0, MapWidth);
+            int zRand = Random.Range(0, MapDepth);
+
+            if (objmapArr[xRand, (int)heightArr[xRand, zRand]+1, zRand] == ObjType.None)
+            {
+                objmapArr[xRand, (int)heightArr[xRand, zRand] + 1, zRand] = ObjType.Rock;
+            }
+        }
+    }
+    
     void WaterGenerate()
     {
         for (int x = 0; x < MapWidth; x++)
@@ -70,7 +145,8 @@ public class MapGenerator : MonoBehaviour {
             {
                 for (int y = (int)heightArr[x, z]+1; y < WaterLevelHeight; y++)
                 {
-                    GameObject newBlock = Instantiate(Blocks[5], new Vector3(x, y, z), Quaternion.identity, Map.transform);
+                    if(objmapArr[x,y,z]==ObjType.None)
+                        objmapArr[x, y, z] = ObjType.Water;
                 }
             }
         }
