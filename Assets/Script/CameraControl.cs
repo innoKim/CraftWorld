544072e5a6d27a63e0824789720c0607d76cc24b
DESKTOP-H544JN2\idhik <idhik@naver.com>
2017-11-10 15:06:30 +0900

    voxel 로 직접 메쉬를 만들고 blender로 본을 심어 만든 플레이어 추가

--- Assets/Script/CameraControl.cs
+++ Assets/Script/CameraControl.cs
@@ -9,22 +9,26 @@ public class CameraControl : MonoBehaviour {
     private Vector3 offSet;
     private Vector3 curPosition;
     private Quaternion curRot;
+    private float rotSpd;
+    private float rot = 0.0f;
     // Use this for initialization
     void Start()
     {
         curPosition = targetTransform.position;
         offSet = this.transform.position;
         curRot = transform.rotation;
+        rotSpd = targetTransform.gameObject.GetComponent<PlayerController>().RotSpd;
     }
 
     // Update is called once per frame
     void Update()
     {
+        rot += Input.GetAxisRaw("Horizontal") * rotSpd * Time.deltaTime;
+
         if (targetTransform)
         {
-            transform.position = curPosition + targetTransform.rotation* offSet;
-            transform.rotation = targetTransform.rotation*curRot;
-
+            transform.position = targetTransform.position + transform.rotation* offSet;
+            transform.rotation = Quaternion.Euler(new Vector3(0,rot,0)) * curRot;
             curPosition = targetTransform.position * 0.05f + curPosition * 0.95f;
         }
     }
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float MoveSpd;
+    private float spd;
+
+    public float RotSpd;
+    public float JumpPower;
+
+
+    private Rigidbody rb;
+    private Animator anim;
+
+    public bool isGround;
+    public bool isWater;
+
+    public Quaternion lookDirection;
+    public Transform CameraTransform;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        
+        ObjectManager.Instance.player = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        Jump();
+
+        AnimationSetting();
+    }
+
+    void AnimationSetting()
+    {
+        anim.SetFloat("Speed", spd);
+    }
+
+    void Jump()
+    {
+        if(isGround&&Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * JumpPower);
+            isGround = false;
+        }
+        else if(isWater && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * JumpPower);
+        }
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) spd = Input.GetAxisRaw("Vertical") * MoveSpd * 2.0f;
+        else spd = Input.GetAxisRaw("Vertical") * MoveSpd;
+
+        //if (Mathf.Abs(spd) > 0.1f) transform.rotation = lookDirection;
+
+        transform.position += transform.forward * spd * Time.deltaTime;
+        //lookDirection = lookDirection * Quaternion.Euler(new Vector3(0, Input.GetAxisRaw("Horizontal") * RotSpd * Time.deltaTime, 0));
+        //transform.Rotate(new Vector3(0, Input.GetAxisRaw("Horizontal") * RotSpd * Time.deltaTime, 0));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Terrain"))
+        {
+            isGround = true;
+        }
+        if(other.CompareTag("Water"))
+        {
+            isWater = true;
+            rb.drag = 5.0f;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            isWater = false;
+            rb.drag = 0.0f;
+        }
+    }
+
+    
+}
