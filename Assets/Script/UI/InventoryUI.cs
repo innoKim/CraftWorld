3692513d62710269e3d549ae9fa5e8cca139e8bd
DESKTOP-H544JN2\idhik <idhik@naver.com>
2017-11-21 14:56:32 +0900

    크크

--- Assets/Script/UI/InventoryUI.cs
+++ Assets/Script/UI/InventoryUI.cs
@@ -9,16 +9,17 @@ public class InventoryUI : MonoBehaviour {
     List<GameObject> holders;
     
 	void Start () {
+
         itemHolder = Resources.Load<GameObject>("Prefab/UI/ItemHolder");
         itemHolder.GetComponent<Image>().sprite = transform.parent.GetComponent<Image>().sprite;
 
         holders = new List<GameObject>();
-        for(int i=0;i<15;i++)
+
+        for (int i=0;i<15;i++)
         {
             GameObject newHolder = Instantiate(itemHolder, this.transform);
             newHolder.name = "ItemHolder" + i.ToString();
             holders.Add(newHolder);
         }
     }
-
 }
--- Assets/Script/UI/ItemHolder.cs
+++ Assets/Script/UI/ItemHolder.cs
@@ -5,19 +5,41 @@ using UnityEngine.UI;
 
 public class ItemHolder : MonoBehaviour {
 
-    GameObject texture;
-    RawImage rawImage;
-    string itemName;
+
+    public GameObject holdItem;
+    public string itemName;
     int count;
 
-	// Use this for initialization
-	void Start () {
-        texture = new GameObject("texture");
-        texture.AddComponent<RawImage>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+        ItemHold();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void ItemHold()
+    {
+        holdItem = ItemManager.Instance.GetItemFromPool(itemName);
+
+
+        if (holdItem == null) return;
+        holdItem.transform.parent = this.transform;
+
+        RectTransform rectTransform = holdItem.AddComponent<RectTransform>();
+
+        rectTransform.localPosition = new Vector3(0, 0, -200);
+        rectTransform.localScale = new Vector3(100, 100, 100);
+        rectTransform.localRotation = Quaternion.Euler(new Vector3(-30, 30, 0));        
+
+        Destroy(holdItem.GetComponent<Rigidbody>());
+    }
+       
+
+    void ItemDehold()
+    {
+
+    }
 }
